04eb6c1b0f4a4215e2c3efba0f6a09c0cee02133
prolove15 <blight1115.dev@gmail.com>
2024-03-13 22:57:12 -0700

    completed

---
+++ Assets/Card Game/Scripts/Networking/Default/DefaultBridge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CardGame.Networking.Default {
+    public class DefaultBridge : Gateway {
+        private void ThrowNetworkingError () {
+            throw new NotImplementedException("[DefaultBridge] Networking is not implemented on this gateway.");
+        }
+        public override void Connect() {
+            ThrowNetworkingError();
+        }
+
+        public override void QuickMatch() {
+            ThrowNetworkingError();
+        }
+    }
+}
